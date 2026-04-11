a55dbe1326ca5ecf5e3413c56ad59fae8cb97458
denizabva <13569@holbertonstudents.com>
2026-04-11 16:46:08 +0000

    Fix MyList implementation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,8 +5,10 @@ Module that defines a class MyList that inherits from list.
 
 
 class MyList(list):
-    """Custom list that can print sorted version."""
+    """Custom list that can print sorted values."""
 
     def print_sorted(self):
-        """Prints the list sorted in ascending order."""
-        print(sorted(self))
+        """Prints the list in ascending order."""
+        new_list = self.copy()
+        new_list.sort()
+        print(new_list)
