065b821b7e1314557ba1056447dae9256c74ce47
denizabva <13569@holbertonstudents.com>
2026-05-30 20:18:10 +0400

    Create 9-cities_by_state_join.sql

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- List all cities with their state names using a single SELECT statement
+
+SELECT
+    cities.id,
+    cities.name,
+    states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
