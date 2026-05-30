abddc40c89787fdab512cd45764a420fed670dd1
denizabva <13569@holbertonstudents.com>
2026-05-30 20:06:44 +0400

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,6 @@
--- Create user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SELECT user, host FROM mysql.user;
+-- Create user and set password, do nothing if it already exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Set password (safe even if user already exists)
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Give all privileges
+-- Give all privileges on the server
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
